6b78c11a164f1e86d6c84777dfa64a941b16729d
Denes <csaszar.denes@gmail.com>
2023-03-28 01:34:59 +0200

    task 4

---
+++ js/darkmode.js
@@ -0,0 +1,21 @@
+// // const toogle = document.querySelector('[data-js="toogle"]');
+
+// // function switchTheme() {
+// //   const rootElement = document.documentElement;
+
+// //   let dataTheme = rootElement.getAttribute("data-theme");
+
+// //   let newTheme;
+
+// //   if (dataTheme === "light") {
+// //     newTheme = "dark";
+// //   } else {
+// //     newTheme = "light";
+// //   }
+
+// //   rootElement.setAttribute("data-theme", newTheme);
+// // }
+
+// // toogle.addEventListener("change", () => {
+// //   switchTheme();
+// });
